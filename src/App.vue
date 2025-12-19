1936d341e188c5cbb9146c2d4b2561d24027a008
feige996 <1020102647@qq.com>
2025-12-19 14:48:50 +0800

    refactor: 将本地视频资源替换为远程资源

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import VideoTimeline from '@/components/VideoTimeline.vue'
 
-import longVideo from '@/assets/long.mp4' // 本地视频资源
+// import longVideo from '@/assets/long.mp4' // 本地视频资源
+const longVideo = 'https://oss.laf.run/ukw0y1-site/beautiful-girl-with-audio.mp4' // 远程资源
 </script>
 
 <template>
