ad954cf999e0532c9d91414d220f18fe6e3dd608
feige996 <1020102647@qq.com>
2025-12-21 11:05:00 +0800

    feat: 添加横图视频资源并更新视频切换逻辑
    更新 App.vue 以使用新的横图视频资源，并重构视频切换功能。同时更新 README.md 添加性能测试数据。

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script lang="ts" setup>
 import VideoContainer from './components/VideoContainer.vue'
 
-import localVideo from '@/assets/long.mp4' // 本地视频资源
+import longVideo from '@/assets/long.mp4' // 本地视频资源
 import shortVideo from '@/assets/10s.mp4' // 本地视频资源
+import hengtu1 from '@/assets/hengtu1.mp4' // 本地视频资源
+
+const localVideo = hengtu1
 const remoteVideo = 'https://oss.laf.run/ukw0y1-site/beautiful-girl-with-audio.mp4' // 远程资源
 
-const longVideo = ref(shortVideo)
+const videoUrl = ref(localVideo)
 const toggleVideo = () => {
-  longVideo.value = longVideo.value === localVideo ? remoteVideo : shortVideo
+  videoUrl.value = videoUrl.value === localVideo ? remoteVideo : localVideo
 }
 </script>
 
 <template>
   <div>
     <button @click="toggleVideo">切换视频</button>
-    <span>{{ longVideo }}</span>
-    <VideoContainer :videoUrl="longVideo" />
+    <span>{{ videoUrl }}</span>
+    <VideoContainer :videoUrl="videoUrl" />
   </div>
 </template>
